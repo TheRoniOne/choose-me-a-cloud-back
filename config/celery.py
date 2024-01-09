24600322cce3049c695736b81ae87ac6e1f9f9ce
TheRoniOne <ronidani99@hotmail.com>
2024-01-08 23:29:37 -0500

    Minor improvements

--- config/celery.py
+++ config/celery.py
@@ -1,7 +1,16 @@
+from os import environ
+
 from celery import Celery
+
+environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 app = Celery("proj")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f"Request: {self.request!r}")

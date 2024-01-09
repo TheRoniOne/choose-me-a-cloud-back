cf46598a4b0f5fcd8b015af553bd62c63192117c
TheRoniOne <ronidani99@hotmail.com>
2024-01-08 23:33:56 -0500

    Add celery config for tests

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,6 +1,14 @@
 from .base import *  # NOSONAR
 from .base import BASE_DIR
 
+# CELERY
+# ------------------------------------------------------------------------------
+# https://docs.celeryproject.org/en/stable/userguide/configuration.html#configuration
+CELERY_BROKER_FAKE = True
+BROKER_BACKEND = "memory"
+CELERY_TASK_ALWAYS_EAGER = True
+CELERY_TASK_EAGER_PROPAGATES = True
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",

7912d154cea1c6b73beadf530bb8bba3c9471e22
TheRoniOne <ronidani99@hotmail.com>
2024-02-09 21:40:31 -0500

    Minor fix

--- clouds/apis/urls.py
+++ clouds/apis/urls.py
@@ -20,7 +20,7 @@ router.register("product", ProductViewSet, basename="product")
 
 urlpatterns = [
     re_path("", include(router.urls)),
-    re_path("vendor/names", VendorNameListAPIView.as_view(), name="vendor_names"),
-    re_path("cloud/names", CloudNameListAPIView.as_view(), name="cloud_names"),
-    re_path("product/names", ProductNameListAPIView.as_view(), name="product_names"),
+    re_path("names/vendor", VendorNameListAPIView.as_view(), name="vendor_names"),
+    re_path("names/cloud", CloudNameListAPIView.as_view(), name="cloud_names"),
+    re_path("names/product", ProductNameListAPIView.as_view(), name="product_names"),
 ]
